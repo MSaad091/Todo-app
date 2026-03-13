1283462e1eff9c50e6cc5371f68da07e27cf42f0
M Saad Mustafa <msaadmustafa091@gmail.com>
2026-03-13 10:02:05 -0700

    update urls

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const api = axios.create({
-    baseURL:"http://localhost:4000/user",
+    baseURL:"https://todo-app-oohs.onrender.com/user",
     withCredentials:true
 })
 
